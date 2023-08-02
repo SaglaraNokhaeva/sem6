a1cbba970e3a4e4418470f2ca7af860d9518f0ee
Saglara <nohaevass@yandex.ru>
2023-08-02 19:42:54 +0300

    1

--- my_games/DZ_ex2_queen.py
+++ my_games/DZ_ex2_queen.py
@@ -2,13 +2,30 @@ def queens(my_str):
 
 # Разбиваем строку в список, содержащий позиции ферзей
     _ = my_str.split()
-    newlist = []
+    list_position = []
     for x in range(0, 16, 2):
         innerlist = []
         innerlist.append(_[x])
         innerlist.append(_[x+1])
-        newlist.append(innerlist)
-    print(newlist)
+        list_position.append(innerlist)
+    print(list_position)
+
+# Проверяем бьют ли ферзи друг друга
+    flag = False
+    for i in range(len(list_position)-1):
+        for j in range(i+1, len(list_position)):
+            if list_position[i][0] == list_position[j][0] or list_position[i][1] == list_position[j][1] or abs(list_position[i][0] - list_position[j][0]) == abs(list_position[i][1] - list_position[j][1]):
+                flag = True
+    if flag:
+        result = 'бьёт'
+    else:
+        result = 'не бьёт'
+
+    return result
+
+
+
+
 
 
 
