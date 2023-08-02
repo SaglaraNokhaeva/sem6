c506dc48d3fdd1b4502aa992f3ca6a2ba2cf2e48
Saglara <nohaevass@yandex.ru>
2023-08-02 19:29:01 +0300

    1

--- my_games/DZ_ex2_queen.py
+++ my_games/DZ_ex2_queen.py
@@ -1,1 +1,17 @@
-def queens (str):
+def queens(my_str):
+
+# Разбиваем строку в список, содержащий позиции ферзей
+    _ = my_str.split()
+    newlist = []
+    for x in range(0, 16, 2):
+        innerlist = []
+        innerlist.append(_[x])
+        innerlist.append(_[x+1])
+        newlist.append(innerlist)
+    print(newlist)
+
+
+
+
+
+queens('1 2 7 3 4 5 7 9 2 3 4 5 6 8 2 1')
